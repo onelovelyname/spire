f1f6009fd4f3d16d91464544799488094abcec69
Melanie Gin <melaniegin1@gmail.com>
2015-07-09 12:33:50 -0700

    install npm module cron to run cron job every day

---
+++ server/config/cron.js
@@ -0,0 +1,7 @@
+var CronJob = require('cron').CronJob;
+
+var job = new CronJob('0 0 * * *', function() {
+  console.log('You will see this message every second');
+}, null, false, 'America/Los_Angeles');
+
+module.exports = job;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var middleware = require('./config/middleware.js');
 var app = express();
+var job = require('./config/cron.js');
 
 middleware(app, express);
 
 var db = require('./db.js');
 
 module.exports = app;
+
+job.start();
