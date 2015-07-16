b722b93b6cb9ee4eeb68ba29fde204f9e4a77a81
Melanie Gin <melaniegin1@gmail.com>
2015-07-16 09:35:04 -0700

    set up for Passport login

--- client/views/LoginView.js
+++ client/views/LoginView.js
@@ -2,9 +2,20 @@ var app = app || {};
 
 app.LoginView = Marionette.ItemView.extend({
 
-  tagName: "section",
+  tagName: "form",
 
   template: Handlebars.compile($('#loginTemplate').html()),
+
+  events: {
+
+    "submit": "signin"
+
+  },
+
+  signin: function() {
+    event.preventDefault();
+    console.log("showing signin");
+  }
 
 });
 
--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,10 +1,37 @@
 var bodyParser = require('body-parser');
 var appController = require('../app/appController.js');
+var passport = require('passport');
+var GitHubStrategy = require('passport-github2').Strategy;
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+var GITHUB_CLIENT_ID = '8482186f19648d881478';
+var GITHUB_CLIENT_SECRET = 'ed4ba6d32892d75b86486f38d03ec56dc9157005';
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/api/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+));
+
 
 module.exports = function(app, express) {
 
   var router = express.Router();
 
+  app.use(cookieParser());
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
 
@@ -13,7 +40,6 @@ module.exports = function(app, express) {
   router.put('/habits', appController.updateHabitStatus);
 
   router.get('/habitCompletion', appController.fetchHabitCompletion);
-  //app.post('/api/habitCompletion', appController.createHabitCompletion);
 
   app.use('/api', router);
 
