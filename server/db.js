5544892194c9081a6f7622fe3a07e3b5a5af4fac
Melanie Gin <melaniegin1@gmail.com>
2015-07-16 15:13:51 -0700

    create users table in db

--- server/db.js
+++ server/db.js
@@ -16,41 +16,45 @@ var db = require('bookshelf')(knex);
 
 db.plugin('registry');
 
-db.knex.schema.hasTable('habits').then(function(exists){
+db.knex.schema.hasTable('users').then(function(exists) {
   if(!exists) {
-    db.knex.schema.createTable('habits', function(habit){
-      habit.increments('id').primary();
-      habit.string('action', 100);
-      habit.integer('quantity', 10);
-      habit.timestamp('timestamp', 30);
+    db.knex.schema.createTable('users', function(user) {
+      user.increments('id').primary();
+      user.integer('github_id', 20);
+      user.string('name', 30);
+      user.string('email', 30);
     }).then(function(table) {
-      console.log("Created Habits Table", table);
-
-      db.knex.schema.hasTable('completions').then(function(exists){
+      console.log("Created Users Table");
+      
+      db.knex.schema.hasTable('habits').then(function(exists){
         if(!exists) {
-          db.knex.schema.createTable('completions', function(habitCompletion){
-            habitCompletion.increments('id').primary();
-            habitCompletion.integer('habit_id').references('habits.id');
-            habitCompletion.date('start_date');
-            habitCompletion.date('end_date');
-            habitCompletion.float('status');
-          }).then(function(table){
-            console.log('Created Completions Table', table);
-            // db.knex.schema.hasTable('completions_habits').then(function(exists){
-            //   if(!exists) {
-            //     db.knex.schema.createTable('completions_habits', function(habit_completion){
-            //       habit_completion.integer('habit_id').references('habits.id');
-            //       habit_completion.integer('completion_id').references('completions.id');
-            //     }).then(function(table){
-            //       console.log("Created Habits Completions Table", table);
-            //     });
-            //   }
-            // });
+          db.knex.schema.createTable('habits', function(habit){
+            habit.increments('id').primary();
+            habit.string('action', 100);
+            habit.integer('quantity', 10);
+            habit.timestamp('timestamp', 30);
+          }).then(function(table) {
+            console.log("Created Habits Table", table);
+
+            db.knex.schema.hasTable('completions').then(function(exists){
+              if(!exists) {
+                db.knex.schema.createTable('completions', function(habitCompletion){
+                  habitCompletion.increments('id').primary();
+                  habitCompletion.integer('habit_id').references('habits.id');
+                  habitCompletion.date('start_date');
+                  habitCompletion.date('end_date');
+                  habitCompletion.float('status');
+                }).then(function(table){
+                  console.log('Created Completions Table', table);
+                });
+              }
+            });
           });
         }
       });
     });
   }
 });
+
 
 module.exports = db;
