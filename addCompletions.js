a879af371c96a52e3207e8875edf24197a76ee7f
Melanie Gin <melaniegin1@gmail.com>
2015-07-29 15:56:55 -0700

    update for Heroku scheduler

---
+++ addCompletions.js
@@ -0,0 +1,4 @@
+var appController = require('../server/app/appController.js');
+
+console.log('If you see this message, the scheduled Heroku job works!');
+appController.createRecurringHabitCompletion();
