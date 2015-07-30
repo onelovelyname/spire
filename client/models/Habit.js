da68f42e7d8851bb060c303ec9e9d86fa306ea13
Melanie Gin <melaniegin1@gmail.com>
2015-07-29 19:12:49 -0700

    Revert "update for Heroku"
    This reverts commit aa11674fca8806048c5f14db36f8d70f1a0eddd2.

--- client/models/Habit.js
+++ client/models/Habit.js
@@ -25,9 +25,9 @@ app.Habit = Backbone.Model.extend({
       console.log("today in updateCompletions: ", today);
       
       var start = completion.get('start_date');
-      console.log("parsed completion.get('start_date'): ", Date.parse(start));
+      console.log("completion.get('start_date'): ", start);
 
-      console.log("today === start", today === Date.parse(start));
+      console.log("today === start", today === start);
 
       if(Date.parse(completion.get('start_date')) === today) {
 
